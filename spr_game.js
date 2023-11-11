a523bd62a68f639f88c3d47f047d46998ae29774
irsc <rivero.scastilla@gmail.com>
2023-11-11 20:36:19 +0100

    Added game function

--- spr_game.js
+++ spr_game.js
@@ -11,40 +11,81 @@ function playRound(playerSelection, computerSelection) {
         case "ROCK":
             switch(computer){
                 case "ROCK":
-                    return "Yay...draw!";
+                    console.log("Yay...draw!");
+                    return "draw";
                 case "PAPER":
-                    return "You loose... Paper beats rock!";
+                    console.log("Paper wraps and beats rock!");
+                    return "computer";
                 case "SCISSORS":
-                    return "You win!Rock smashes scissors";
+                    console.log("Rock smashes scissors!");
+                    return "player";
             }
             break
         case "PAPER":
             switch(computer){
                 case "ROCK":
-                    return "You win!Paper wraps and beats rock";
+                    console.log("Paper wraps and beats rock!");
+                    return "player";
                 case "PAPER":
-                    return "Yay...draw!";
+                    console.log("Yay...draw!");
+                    return "draw";
                 case "SCISSORS":
-                    return "You loose...scissors cut paper";
+                    console.log("Scissors cut paper!");
+                    return "computer";
             }
             break
         case "SCISSORS":
             switch(computer){
                 case "ROCK":
-                    return "You loose... Rock smashes scissors!";
+                    console.log("Rock smashes scissors!");
+                    return "computer"
                 case "PAPER":
-                    return "You win! Scissors cut paper";
+                    console.log("Scissors cut paper!");
+                    return "player";
                 case "SCISSORS":
-                    return "Yay...draw!";
+                    console.log("Yay...draw!");
+                    return "draw";
             }
             break
     }
   }
-   
- /*  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection)); */
 
 function game(){
-    
+    let match = 0;
+    let playerWin = 0;
+    let computerWin = 0;
+    let roundWin;
+
+    while(match < 5){
+
+        const playerSelection = prompt("Please enter Rock / Scissors /Paper. Which one you would like to play: ");
+        const computerSelection = getComputerChoice();
+        console.log("Player plays ",playerSelection,"; Computer plays ",computerSelection);
+        roundWin = playRound(playerSelection,computerSelection);
+
+        if( roundWin == "player"){
+            playerWin++;
+            console.log("You win round ",match+1,"!");
+        }else if( roundWin == "computer"){
+            computerWin++;
+            console.log("Computer wins round ",match+1,"!");
+        }else if(roundWin == "draw"){
+            console.log("No winner in round ",match+1,"!");
+        }
+        match++;
+    }
+
+    console.log("Total wins for player: ",playerWin);
+    console.log("Total wins for computer: ",computerWin);
+
+    if(playerWin > computerWin ){
+        console.log("You are the overall winner");
+    }else if(playerWin < computerWin ){
+        console.log("The computer is the overal winner");
+    } else{
+        console.log("You've drawn...No winner this time!");
+    }
+
 }
+
+game();
